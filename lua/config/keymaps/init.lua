ad4ab946eae65bc6eb139c2ecde0e22118a4ac6a
yufo211 <95125392+yufo211@users.noreply.github.com>
2026-04-26 12:59:42 +0900

    deleted LSP keymappings with leader key

--- lua/config/keymaps/init.lua
+++ lua/config/keymaps/init.lua
@@ -42,19 +42,11 @@ vim.keymap.set({ "n", "x" }, "C", '"_C', { noremap = true })
 vim.keymap.set({ "n", "x" }, "s", '"_s', { noremap = true })
 vim.keymap.set({ "n", "x" }, "S", '"_S', { noremap = true })
 
--- プラグイン
+-- プラグイン関連
 -- nvim-telescope/telescope.nvim
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: live grep" })
+vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope: keymappings" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: help tags" })
-
--- neovim/nvim-lspconfig
-vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { desc = "LSP: go to definition" })
-vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "LSP: hover" })
-vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, { desc = "LSP: go to implementation" })
-vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { desc = "LSP: show references" })
-vim.keymap.set("n", "<leader>lR", vim.lsp.buf.rename, { desc = "LSP: rename" })
-vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "LSP: code action" })
-vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float, { desc = "LSP: show diagnostics" })
