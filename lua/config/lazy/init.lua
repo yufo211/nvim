d0ff3c7d8fd599e9f857ac79af5061121a0d0d2c
yufo211 <95125392+yufo211@users.noreply.github.com>
2026-04-26 18:09:22 +0900

    disable luarocks

--- lua/config/lazy/init.lua
+++ lua/config/lazy/init.lua
@@ -114,4 +114,8 @@ require("lazy").setup({
 	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
+	-- disable luarocks
+	rocks = {
+		enabled = false,
+	},
 })
