8098667367f27b9ee85d2d97c40bb98a440c2711
yufo211 <95125392+yufo211@users.noreply.github.com>
2026-06-08 11:47:57 +0900

    Exclude file type settings

--- lua/config/plugins/stevearc/conform.lua
+++ lua/config/plugins/stevearc/conform.lua
@@ -1,17 +1,4 @@
 -- stevearc/conform.nvim
-require("conform").setup({
-	formatters_by_ft = {
-		css = { "prettier", stop_after_first = true },
-		javascript = { "prettier", stop_after_first = true },
-		json = { "prettier", stop_after_first = true },
-		html = { "prettier", stop_after_first = true },
-		lua = { "stylua" },
-		markdown = { "prettier", stop_after_first = true },
-		ruby = { "rubocop", stop_after_first = true },
-		typescript = { "prettier", stop_after_first = true },
-		yaml = { "prettier", "yamlfmt", stop_after_first = true },
-	},
-})
 
 -- format on save autocmd
 vim.api.nvim_create_autocmd("BufWritePre", {
